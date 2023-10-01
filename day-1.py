5e3ca1baf806587e9272fd99546aa988e1920b78
reumareis <146622409+reumareis@users.noreply.github.com>
2023-10-01 11:27:14 +0300

    Add files via upload
    this is first days project

---
+++ day-1.py
@@ -0,0 +1,37 @@
+"""print("reis behru ahmed ")
+print(len  (input("what is your name?")))
+print(len (input(" sorre sewoch ")))
+print ("hello" + input("what is yoour name ?")+" what can i help you !")
+#name = "reis behru"
+#print(name)
+print("your id nember have "+ input("enter your id nember " ))  
+print(input("reis behru ahmed"))
+print(len (input("reis behru" ))) 
+len (input("reis behru" ))
+#len(name)
+len("reis" )
+a=input("a")
+b=input("b")
+c=a
+a= b
+b=c
+print("a=" + a)
+print("b=" +b)"""
+#day one project 
+#1.creat a greetingnfor your program.
+#2.ask the user for the city that they grew up in 
+#3.ask the user for the name of apet 
+#4.combine the name of their city and pet and show them their band name 
+print("wellcome to our project!!!")
+city=input("what is name of the city you grew up ?\n")
+pet=input("what is your pet`s name ?\n")
+print("your band name could be " + (city)+" "+ (pet))
+#day one 
+
+
+
+
+
+
+
+
